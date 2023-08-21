562daf13b26a4e0f436145f6f4669bec7f7af313
manue <manu.svent@gmail.com>
2023-08-21 10:23:39 -0300

    tarefa02

---
+++ src/t02.java
@@ -0,0 +1,9 @@
+public class t02 {
+    public static void main(String[] args) {
+        String texto = "Desenvolvimento é muito legal!";
+
+        String nova = texto.replace("Desenvolvimento","Programação");
+        System.out.println(nova);
+
+    }
+}
